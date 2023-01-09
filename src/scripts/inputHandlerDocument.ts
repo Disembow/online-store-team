7c6d4473e21a1fb92ebe105e98dd8d4700220673
KirillGenin <332211op@gmail.com>
2023-01-09 16:40:38 +0300

    fix: check instanceof input

--- src/scripts/inputHandlerDocument.ts
+++ src/scripts/inputHandlerDocument.ts
@@ -3,18 +3,19 @@ import app from './app';
 
 export default function inputHandlerDocument(event: Event): void {
   const target = event.target;
+  if (!target) throw new Error('target is null or not input');
 
-  if (!target || !(target instanceof HTMLInputElement)) throw new Error('target is null or not input');
+  if (target instanceof HTMLInputElement) {
+    if (target.name === 'category' || target.name === 'brand') {
+      app.filterCheckbox(target.name, target.value, target.checked);
+    }
 
-  if (target.name === 'category' || target.name === 'brand') {
-    app.filterCheckbox(target.name, target.value, target.checked);
-  }
+    if (target.classList.contains('dual-slider__input')) {
+      controlDualSlider(target);
+    }
 
-  if (target.classList.contains('dual-slider__input')) {
-    controlDualSlider(target);
-  }
-
-  if (target.id === 'search-input') {
-    app.filterSearch(target.value);
+    if (target.id === 'search-input') {
+      app.filterSearch(target.value);
+    }
   }
 }
